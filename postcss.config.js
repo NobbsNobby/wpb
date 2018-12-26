d4b869478df7125b7754ae79e2fbed28b3be12da
NobbsNobby <art.kotov1@gmail.com>
2018-12-26 12:18:57 +0300

    add font smooth to css 2

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,7 @@ module.exports = {
     "postcss-import": {},
     "postcss-font-smoothing": {},
     "postcss-easing-gradients": {},
+    "postcss-nested": {},
     "postcss-preset-env": {
       stage: 0,
       preserve: false //Удалять свойства недоступные для данной настройки **Только для stage 0**
